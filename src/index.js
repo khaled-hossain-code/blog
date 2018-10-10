f39a338070ac43726282d6e7d8367750e74cdf87
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-10-10 11:10:35 +0600

    Add Posts New route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import promise from 'redux-promise';
 
 import reducers from "./reducers";
 
-import PostIndex from './containers/PostIndex';
+import PostIndex from "./containers/PostIndex";
+import PostNew from "./containers/PostNew";
+
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -15,7 +17,8 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostIndex} />
+        <Route exact path="/" component={PostIndex} />
+        <Route path="/posts/new" component={PostNew} />
       </div>
     </BrowserRouter>
   </Provider>,
